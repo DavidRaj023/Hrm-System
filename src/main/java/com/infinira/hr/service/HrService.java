f731d4f0598bf5ff3b4857d96df6ec46393fac79
DavidRaj023 <benjamindavid023@gmail.com>
2021-04-26 10:32:01 +0530

    🔨 Service class added

---
+++ src/main/java/com/infinira/hr/service/HrService.java
@@ -0,0 +1,26 @@
+package com.infinira.hr.service;
+import com.infinira.hr.model.Employee;
+import com.infinira.hr.dao.EmployeeDao;
+import java.util.List;
+
+public class HrService implements HrInterface {
+    public int createEmployee( Employee employee ){
+        return new EmployeeDao().create(employee);
+    }
+    
+    public Employee getEmployee( int employeeId ){
+        return new EmployeeDao().get(employeeId);
+    }
+    
+    public void updateEmployee( Employee employee ){
+        new EmployeeDao().update(employee);
+    }
+    
+    public void deleteEmployee( int employeeId ){
+        new EmployeeDao().delete(employeeId);
+    }
+    
+    public List<Employee> getAllEmployees(){
+        return new EmployeeDao().getAll();
+    }
+}
