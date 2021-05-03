d3ab8847c1e5a3031b1744942f0e123bd70dcb39
DavidRaj023 <benjamindavid023@gmail.com>
2021-05-03 18:37:40 +0530

    Add anotaion in hr controller

--- src/main/java/com/infinira/hr/controller/HrController.java
+++ src/main/java/com/infinira/hr/controller/HrController.java
@@ -2,6 +2,13 @@ package com.infinira.hr.controller;
 
 import com.infinira.hr.model.Employee;
 import com.infinira.hr.service.HrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
@@ -9,24 +16,30 @@ import java.util.List;
 @RestController
 public class HrController {
 
+	@Autowired
     private HrService service;
     
+	@PostMapping("/create")
     public int create(@RequestBody Employee employee){
         return service.createEmployee(employee);
     }
-
+	
+	@GetMapping("/get/{id}")
     public Employee get(@PathVariable int id){
         return service.getEmployee(id);
     }
     
+	@GetMapping("/getAll")
     public List getAll(){
         return service.getAllEmployees();
     }
     
+	@PutMapping("/update")
     public void update(@RequestBody Employee employee){
         service.updateEmployee(employee);
     }
     
+	@DeleteMapping("/delete/{id}")
     public void delete(@PathVariable int id){
         service.deleteEmployee(id);
     }
